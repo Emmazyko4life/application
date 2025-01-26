7cb4f2935dc5d6342b0587128dd6362a8eee5b0f
Emmazyko4life <Eonyemaobi328@gmail>
2025-01-26 20:49:47 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const port = 8050;
+const sequelize = require('./database/sequelize');
+
+
+const app = express();
+app.use(express.json())
+
+
+const server = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+server()
+
+
+app.listen(port, () => {
+console.log(`server is running on port: ${port}`)
+
+})
